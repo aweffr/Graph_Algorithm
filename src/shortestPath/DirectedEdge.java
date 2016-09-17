cd01aa3ad42eb5f13c9b7472cb9356bdf0e4cbcf
aweffr <aweffr@qq.com>
2016-09-17 21:46:16 +0800

    Dijkstra

---
+++ src/shortestPath/DirectedEdge.java
@@ -0,0 +1,34 @@
+package shortestPath;
+/*
+ * In fact the DirectedEdge implementation is simpler than the UndirectedEdge in MST problem
+ * because the two vertices are distinguished.
+ * It use w = e.to(), v = e.from(); to access a DirectedEdge's two vertices.
+ */
+
+public class DirectedEdge {
+	private final int v;
+	private final int w;
+	private final double weight;
+	
+	public DirectedEdge(int v, int w, double weight) {
+		this.v = v;
+		this.w = w;
+		this.weight = weight;
+	}
+	
+	public double weight() {
+		return weight;
+	}
+	
+	public int from() {
+		return v;
+	}
+	
+	public int to() {
+		return w;
+	}
+	
+	public String toString() {
+		return String.format("%d->%d %.2f", v, w, weight);
+	}
+}
